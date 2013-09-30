cd11735b35ff65e29aebc190039767183b735145
David Toy <d@vidtoy.co.uk>
2013-09-30 11:10:07 +0100

    branch for 0.6.0-M2

--- simple-web-cluster/src/main/java/brooklyn/demo/WebClusterDatabaseExampleApp.java
+++ simple-web-cluster/src/main/java/brooklyn/demo/WebClusterDatabaseExampleApp.java
@@ -23,7 +23,7 @@ import brooklyn.entity.basic.StartableApplication;
 import brooklyn.entity.database.mysql.MySqlNode;
 import brooklyn.entity.group.DynamicCluster;
 import brooklyn.entity.java.JavaEntityMethods;
-import brooklyn.entity.proxying.EntitySpecs;
+import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.entity.webapp.ControlledDynamicWebAppCluster;
 import brooklyn.entity.webapp.DynamicWebAppCluster;
 import brooklyn.entity.webapp.JavaWebAppService;
@@ -34,7 +34,9 @@ import brooklyn.event.basic.Sensors;
 import brooklyn.launcher.BrooklynLauncher;
 import brooklyn.location.basic.PortRanges;
 import brooklyn.policy.autoscaling.AutoScalerPolicy;
+import brooklyn.util.BrooklynMavenArtifacts;
 import brooklyn.util.CommandLineUtil;
+import brooklyn.util.ResourceUtils;
 
 import com.google.common.base.Functions;
 import com.google.common.collect.Lists;
@@ -61,17 +63,22 @@ public class WebClusterDatabaseExampleApp extends AbstractApplication implements
     
     public static final String DEFAULT_LOCATION = "localhost";
 
-    public static final String DEFAULT_WAR_PATH = "classpath://hello-world-sql-webapp.war";
+    public static final String DEFAULT_WAR_PATH = new ResourceUtils(WebClusterDatabaseExampleApp.class).
+            // take this war, from the classpath, or via maven if not on the classpath
+            firstAvailableUrl(
+                    "classpath://hello-world-sql-webapp.war",
+                    BrooklynMavenArtifacts.localUrl("example", "brooklyn-example-hello-world-sql-webapp", "war"))
+            .or("classpath://hello-world-sql-webapp.war");
     
     @CatalogConfig(label="WAR (URL)", priority=2)
     public static final ConfigKey<String> WAR_PATH = ConfigKeys.newConfigKey(
         "app.war", "URL to the application archive which should be deployed", 
-        DEFAULT_WAR_PATH);
+        DEFAULT_WAR_PATH);    
 
     // TODO to expose in catalog we need to let the keystore url be specified (not hard)
     // and also confirm that this works for nginx (might be a bit fiddly);
     // booleans in the gui are working (With checkbox)
-//    @CatalogConfig(label="HTTPS")
+    @CatalogConfig(label="HTTPS")
     public static final ConfigKey<Boolean> USE_HTTPS = ConfigKeys.newConfigKey(
             "app.https", "Whether the application should use HTTPS only or just HTTP only (default)", false);
     
@@ -95,14 +102,14 @@ public class WebClusterDatabaseExampleApp extends AbstractApplication implements
     @Override
     public void init() {
         MySqlNode mysql = addChild(
-                EntitySpecs.spec(MySqlNode.class)
+                EntitySpec.create(MySqlNode.class)
                         .configure(MySqlNode.CREATION_SCRIPT_URL, Entities.getRequiredUrlConfig(this, DB_SETUP_SQL_URL)));
 
         ControlledDynamicWebAppCluster web = addChild(
-                EntitySpecs.spec(ControlledDynamicWebAppCluster.class)
+                EntitySpec.create(ControlledDynamicWebAppCluster.class)
                         .configure(WebAppService.HTTP_PORT, PortRanges.fromString("8080+"))
                         // to specify a diferrent appserver:
-//                        .configure(ControlledDynamicWebAppCluster.MEMBER_SPEC, EntitySpecs.spec(TomcatServer.class))
+//                        .configure(ControlledDynamicWebAppCluster.MEMBER_SPEC, EntitySpec.create(TomcatServer.class))
                         .configure(JavaWebAppService.ROOT_WAR, Entities.getRequiredUrlConfig(this, WAR_PATH))
                         .configure(JavaEntityMethods.javaSysProp("brooklyn.example.db.url"), 
                                 formatString("jdbc:%s%s?user=%s\\&password=%s", 
@@ -135,7 +142,7 @@ public class WebClusterDatabaseExampleApp extends AbstractApplication implements
         String location = CommandLineUtil.getCommandLineOption(args, "--location", DEFAULT_LOCATION);
 
         BrooklynLauncher launcher = BrooklynLauncher.newInstance()
-                 .application(EntitySpecs.appSpec(WebClusterDatabaseExampleApp.class)
+                 .application(EntitySpec.create(StartableApplication.class, WebClusterDatabaseExampleApp.class)
                          .displayName("Brooklyn WebApp Cluster with Database example"))
                  .webconsolePort(port)
                  .location(location)
