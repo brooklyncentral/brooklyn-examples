61f8cfb60f57f8a5716230151f6d800fa836c49a
Aled Sage <aled.sage@gmail.com>
2013-10-21 13:03:30 +0100

    branch for 0.6.0-rc.1

--- simple-nosql-cluster/src/main/java/brooklyn/demo/SimpleCassandraCluster.java
+++ simple-nosql-cluster/src/main/java/brooklyn/demo/SimpleCassandraCluster.java
@@ -15,21 +15,41 @@
  */
 package brooklyn.demo;
 
-import brooklyn.entity.basic.ApplicationBuilder;
+import java.util.List;
+
+import brooklyn.entity.basic.AbstractApplication;
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.basic.StartableApplication;
 import brooklyn.entity.nosql.cassandra.CassandraCluster;
 import brooklyn.entity.proxying.EntitySpec;
+import brooklyn.launcher.BrooklynLauncher;
+import brooklyn.util.CommandLineUtil;
 
-/** Cassandra cluster. */
-public class SimpleCassandraCluster extends ApplicationBuilder {
+import com.google.common.collect.Lists;
 
-    /** Create entities. */
-    protected void doBuild() {
+public class SimpleCassandraCluster extends AbstractApplication {
+
+    private static final String DEFAULT_LOCATION = "localhost";
+
+    @Override
+    public void init() {
         addChild(EntitySpec.create(CassandraCluster.class)
-                .configure("initialSize", "2")
-                .configure("clusterName", "Brooklyn")
-                .configure("jmxPort", "11099+")
-                .configure("rmiServerPort", "9001+")
-                .configure("thriftPort", "9160+"));
+                .configure(CassandraCluster.INITIAL_SIZE, 1)
+                .configure(CassandraCluster.CLUSTER_NAME, "Brooklyn"));
     }
-
+    
+    public static void main(String[] argv) {
+        List<String> args = Lists.newArrayList(argv);
+        String port =  CommandLineUtil.getCommandLineOption(args, "--port", "8081+");
+        String location = CommandLineUtil.getCommandLineOption(args, "--location", DEFAULT_LOCATION);
+        
+        BrooklynLauncher launcher = BrooklynLauncher.newInstance()
+                 .application(EntitySpec.create(StartableApplication.class, SimpleCassandraCluster.class)
+                         .displayName("Cassandra"))
+                         .webconsolePort(port)
+                 .location(location)
+                 .start();
+             
+        Entities.dumpInfo(launcher.getApplications());
+    }
 }
